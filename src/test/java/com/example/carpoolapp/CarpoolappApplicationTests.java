89f0d68e40ab6e014fe607455a6fd09b88841620
Marcus Edlund <marcus.edlund@yh.nackademin.se>
2023-10-03 15:59:39 +0200

    Testing Application classes

--- src/test/java/com/example/carpoolapp/CarpoolappApplicationTests.java
+++ src/test/java/com/example/carpoolapp/CarpoolappApplicationTests.java
@@ -3,8 +3,10 @@ package com.example.carpoolapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootApplication
+@SpringBootTest
+@ContextConfiguration(classes = CarpoolappApplicationTests.class)
 class CarpoolappApplicationTests {
 
     @Test
